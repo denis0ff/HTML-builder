79ed979f86a7759fc73bcb655c407d4e15854f71
Denis Popov <popovdw94@gmail.com>
2021-11-03 10:52:16 +0300

    feat: complete a few tasks

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,18 @@
+const fs = require("fs").promises;
+const path = require("path");
 
+async function copyDir(src, dest) {
+  const entries = await fs.readdir(src, { withFileTypes: true });
+  await fs.mkdir(dest, { recursive: true });
+  for (let entry of entries) {
+    const srcPath = path.join(src, entry.name);
+    const destPath = path.join(dest, entry.name);
+    if (entry.isDirectory()) {
+      await copyDir(srcPath, destPath);
+    } else {
+      await fs.copyFile(srcPath, destPath);
+    }
+  }
+}
+
+copyDir(path.join(__dirname, "files"), path.join(__dirname, "files-copy"));
