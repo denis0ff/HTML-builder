067650a9b9ebb61e80017a107944d62c8ffaea41
Denis Popov <89511613+denis0ff@users.noreply.github.com>
2021-11-10 09:23:33 +0300

    fix: update functional for actual node LTS

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 async function copyDir(src, dest) {
   const entries = await fs.readdir(src, { withFileTypes: true });
-  await fs.rmdir(dest, { recursive: true });
+  await fs.rm(dest, { force: true, recursive: true });
   await fs.mkdir(dest, { recursive: true });
   for (let entry of entries) {
     const srcPath = path.join(src, entry.name);
